e772e91f440ddfe2efc4721d91a1485c6e469910
Suchishetty <shettysuchitha27@gmail.com>
2024-11-06 13:13:54 +0530

    Add files via upload

---
+++ relation.c
@@ -0,0 +1,47 @@
+# include<stdio.h>
+
+int main()
+{
+    int a=10,b=20;
+     if(a<b)
+     {
+         printf("%d is greater than %d \n",b,a);
+     }
+    else
+    {
+        printf("%d is greater than %d\n",a,b);
+        
+    }
+    if(a<=b)
+    {
+        printf("%d greater than or equal to %d\n",b,a);
+        
+    }
+    else
+    {
+        printf("%d greater than or equal to %d\n",a,b);
+        
+    }
+    if(a>=b)
+    {
+        printf("%d greater than or equal to %d\n",a,b);
+        
+    }
+    else
+    {
+        printf("%d greater than or equal to %d\n",b,a);
+        
+    }
+    if(a==b)
+    {
+        printf("both the value are equal\n");
+    }
+    else
+    {
+        printf("both are not equal\n");
+    }
+    
+    
+    
+    
+}
